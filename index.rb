99e713705e84727f2cbba22c302ee326d8383e09
Hiren Gondhiya <hirengondhiya@gmail.com>
2019-12-05 15:50:57 +1100

    final product

--- index.rb
+++ index.rb
@@ -1,5 +1,6 @@
 require "artii"
 require "colorize"
+require "tty-prompt"
 
 $participants = {
     "louise" => {
@@ -83,82 +84,106 @@ $participants = {
 }
 
 def clear_screen
+	#sleep is used for how many sec you want to diplay the santa name for
+    # sleep(3)
+    print "\e[H\e[2J"
 end
 
 def get_santa_name
-    current_santa_name = ""
-    # Display list of the participants who have not played so far and allow the current user to identify who s/he is?
+    # Display list of the participants and allow user to identify who s/he is?
+    prompt = TTY::Prompt.new
     # Store the user selection into the variable current_santa_name
+    current_santa_name = prompt.select("Choose the name", $participants.keys)
     return current_santa_name
 end
 
-def match_random_person_for(current_santa_name)
-    # get user properties for current_santa_name
-    # check if current sant is already matched with another participant
-    # if yes 
-    #     return that participant's name
-    # else
-    #     build an array named "persons" with keys from the $partcipants hash who are not picked up by another secret santa pro tip use get_not_picked_participants_except method and pass current santa name as argument
-    #     generate a random number with range input to be length of the array from previous step
-    #     return the person at the random index generated in last step
+def match_random_person_for current_santa_name
+    current_santa_props = fetch_properties_for current_santa_name
+    if (current_santa_props[:is_santa_for] != "")
+        return current_santa_props[:is_santa_for]
+    end
+    # build an array name "persons" with keys of the partcipants hash who are not picked up by a secret santa or current_santa
+    persons = get_not_picked_participants_except current_santa_name
+    # generate a random number with range input to be length of the array from previous step
+    randon_number = rand persons.length
+    return persons[randon_number]
 end
 
 def add_to_file current_santa_name, random_match_name
     # open file called matches.txt
     # append current_santa_name and random_match_name to the file in the format 
     # "<Santa Name>: <Match Name>"
+
+    open('matches.txt', 'a') do |f|
+        f << "#{current_santa_name}: #{random_match_name}\n"
+    end
 end
 
 def display random_match_name, delay
-    # display name of the person on screen in big letters using artii and colorize
-    # sleep for delay seconds
+    # display name of the person on screen in big letters using artii
+    a = Artii::Base.new
+    puts a.asciify(random_match_name).green.on_black
+    # sleep for 15 seconds
+    # sleep 15
+    print "." * delay
+    i=15
+    for i in 1..delay
+        print "\b"*i + " "*i
+        sleep 1
+    end
     # clear screen
+    clear_screen
 end
 
 def fetch_properties_for current_santa_name
     current_santa_props = {}
     # from $participants hash find the value for key matching current_santa_name and store it into variable current_santa
+    current_santa_props = $participants[current_santa_name.downcase]
+    return current_santa_props
 end
 
 def update_properties current_santa_name, matched_random_person_name
-    # get current santa and matched person properties
-    # pro tip use call fetch_properties_for method twice and pass current_santa_name and matched_random_person_name arguments in respective calls. Save returned result in seperate variables
-    # In current santa properties update is_santa_for with name of matched_random_person_name
-    # and set is_santa to true
-    # In matched person properties update my_santa_is with name of current_santa_name
-    # and set is_picked_by_santa to true
+    current_santa_props = fetch_properties_for current_santa_name
+    matched_random_person_props = fetch_properties_for matched_random_person_name
+
+    current_santa_props[:is_santa_for] = matched_random_person_name  
+    current_santa_props[:is_santa] = true  
+    matched_random_person_props[:my_santa_is] = current_santa_name
+    matched_random_person_props[:is_picked_by_santa] = true
 end
 
 def get_remaining_participant_names
-    # This method return name of the participants who has still not played the draw
-    # create an empty array say pnames
-    # using each method fetch participant key and values from $participants hash
-    # for each value if is_santa is false then push the key into pnames array
-    # return the pnames array
+    pnames = []
+    $participants.each { |pname, p_props| 
+        if p_props[:is_santa] == false
+            pnames.push(pname)
+        end
+    }
+    return pnames
 end
 
 def get_not_picked_participants_except current_santa_name
-    # This method returns an array of all participants excluding the current user and those who are not matched with other partcipants
-    # create an empty array say pnames
-    # using each method fetch participant key and values from $participants hash
-    # for each item check if key is not equal to current_santa_name and in value is_picked_by_santa is false then push the key into pnames array
-    # return the pnames array
+    pnames = []
+    $participants.each { |pname, p_props| 
+        if pname.downcase != current_santa_name.downcase && p_props[:is_picked_by_santa] == false
+            pnames.push(pname)
+        end
+    }
+    return pnames
 end
 
 def has_all_participants_played
-    # This method checks if all participants got chance in a draw
-    # pro tip get names of all participants using get_remaining_participant_names function
-    # check if the array returned by above call has no zero values then return true otherwise false
+    pnames = get_remaining_participant_names
+    return pnames.length == 0
 end
 
 def game_play
     while(!has_all_participants_played)
-        clear_screen
         current_santa_name = get_santa_name
         matched_random_person = match_random_person_for current_santa_name
         update_properties current_santa_name, matched_random_person
         add_to_file current_santa_name, matched_random_person
-        display matched_random_person,3
+        display matched_random_person,5
     end
 end
 
